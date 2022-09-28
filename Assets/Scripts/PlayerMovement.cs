3799dd082484e5671ee13d6dfd4b83c93c6b5b39
cacodamen <henryrobinson3400@gmail.com>
2022-09-28 18:44:36 -0400

    Basic Movement
    Movement based on the template game we made in the unity textbook, as a starting point

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,4 +17,32 @@ public class PlayerMovement : MonoBehaviour
     _anim = GetComponent<Animator>();
     _box = GetComponent<BoxCollider2D>();
   }
+
+  void Update()
+  {
+    Vector3 max = _box.bounds.max;
+    Vector3 min = _box.bounds.min;
+    Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+    Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+    Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+    //Horizontal Movement
+    float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+    Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+    _body.velocity = movement;
+
+    //Vertical movement
+    bool grounded = false;
+
+    if (hit != null)
+    {
+      grounded = true;
+    }
+
+    _body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+    if (grounded && Input.GetKeyDown(KeyCode.Space))
+    {
+      _body.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+    }
+  }
 }
