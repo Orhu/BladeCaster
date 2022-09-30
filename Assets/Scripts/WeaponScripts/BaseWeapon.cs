9c5088c6b5f0632c19f9f54d3f854496270ec0c2
cacodamen <henryrobinson3400@gmail.com>
2022-09-30 15:33:09 -0400

    Added BaseWeapon Script
    Calls upon the PlayerValues

---
+++ Assets/Scripts/WeaponScripts/BaseWeapon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeapon : MonoBehaviour
+{
+  public float energyConsump = 1.0f;
+
+  public bool Call()
+  {
+    if(GetComponent<PlayerValues>().UpdateCur(energyConsump))
+    {
+      return false;
+    }
+    return true;
+  }
+}
