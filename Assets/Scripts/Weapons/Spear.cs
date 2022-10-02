e10847668779b2da2e5ccc093c8c8e0f03dcd1c5
cacodamen <henryrobinson3400@gmail.com>
2022-10-02 14:49:39 -0400

    Basic Spear Implement

--- Assets/Scripts/Weapons/Spear.cs
+++ Assets/Scripts/Weapons/Spear.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spear : MonoBehaviour, IWeapon {
-    public int damage {get; private set;}
+    [SerializeField] private LayerMask hurtboxLayerMask;
+    public int damage {get; private set;} = 1;
     public int abilityEnergyCost {get; private set;} = 1;
+
+    private BoxCollider2D _box;
+
+    void Start() {
+        _box = GetComponent<BoxCollider2D>();
+    }
 
     public void WeaponUpdate() {
         // TO DO
     }
 
     public void Attack() {
-        //TO DO
+        float direction = transform.localScale.x; // which way are you facing
+        Collider2D[] enemiesHit = Physics2D.OverlapBoxAll(_box.bounds.center + new Vector3((_box.bounds.extents.x * 3) * direction, 0, 0), new Vector3 (_box.bounds.size.x * 2, _box.bounds.size.y, 0f), 0f, hurtboxLayerMask);
+        foreach (Collider2D hit in enemiesHit) {
+            Debug.Log(hit);
+            // check for tags for different results of getting hit (do later)
+            if (hit.tag == "enemy") {
+                hit.GetComponent<IEnemy>().GetHit(damage, 2.5f * direction);
+            }
+        }
     }
 
-    
+
     public void Ability() {
-        //TO DO
+        //TO DO Implement a charge and vault
+        
     }
 }
