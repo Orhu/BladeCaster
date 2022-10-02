f768e30787e2f3c2a84789faae945ca1a903eea9
cacodamen <henryrobinson3400@gmail.com>
2022-10-02 16:19:16 -0400

    Implemented Basic Items

---
+++ Assets/Scripts/Item/Energy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D other){
+    if(other.gameObject.layer == LayerMask.NameToLayer("Player")){
+      this.gameObject.SetActive(false);
+      //TO DO LATER; Max out Player Energy
+    }
+  }
+}
