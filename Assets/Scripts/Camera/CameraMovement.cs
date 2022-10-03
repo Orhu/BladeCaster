06db2e6ee9d089e64340a5f3edc2303e03f9aed6
cacodamen <henryrobinson3400@gmail.com>
2022-10-02 20:30:23 -0400

    Basic Camera Transition
    NOTE: INSTALL CINEMACHINE

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+  public GameObject camera;
+
+  private void OnTriggerEnter2D(Collider2D other){
+    if(other.CompareTag("Player") && !other.isTrigger){
+      camera.SetActive(true);
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D other){
+    if(other.CompareTag("Player") && !other.isTrigger){
+      camera.SetActive(false);
+    }
+  }
+}
