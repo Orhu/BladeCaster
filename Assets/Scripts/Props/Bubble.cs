7ee7672af9f826b024ca6b3fc0718f4e0c56f763
Orhu <mcdonoughmalcolm@gmail.com>
2022-10-03 21:48:41 -0400

    bubble prefab/respawning

--- Assets/Scripts/Props/Bubble.cs
+++ Assets/Scripts/Props/Bubble.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour, ILevelProp {
     [SerializeField] float knockbackForce;
+    [SerializeField] float respawnTime = 2.5f;
 
     private Animator _anim;
+    private Collider2D _box;
 
     void Start() {
         _anim = GetComponent<Animator>();
+        _box = GetComponent<Collider2D>();
     }
 
     public void Interact() { // pop
         _anim.SetTrigger("pop");
+        StartCoroutine(Respawn());
+    }
+
+    private IEnumerator Respawn() {
+        AnimatorStateInfo stateInfo = _anim.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length*3f);
+        SpriteRenderer _sprite = GetComponent<SpriteRenderer>();
+        _box.enabled = false;
+        _sprite.enabled = false;
+        yield return new WaitForSeconds(respawnTime);
+        _box.enabled = true;
+        _sprite.enabled = true;
     }
 
     public void SwitchOperate() {
